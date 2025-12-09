fd85390e7d200186d610351467c9c19b5ea3e71b
Yohan Ishara <yohanishara01@gmail.com>
2025-12-09 22:18:02 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/social/aisocialcontentgenerator/AiSocialContentGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.social.aisocialcontentgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiSocialContentGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiSocialContentGeneratorApplication.class, args);
+    }
+
+}
