09d935bb05ded0822b108441a1a3d5b21225a53d
Yohan Ishara <yohanishara01@gmail.com>
2025-12-10 21:20:41 +0530

    update files

---
+++ src/main/java/com/social/aisocialcontentgenerator/dto/AuthResponse.java
@@ -0,0 +1,14 @@
+package com.social.aisocialcontentgenerator.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+    private String token;
+}
