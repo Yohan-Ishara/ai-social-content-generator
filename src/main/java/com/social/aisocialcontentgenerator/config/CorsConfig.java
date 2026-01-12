cc316afc46da7a8a68e947fc7c8d261283730fd0
Yohan Ishara <73935858+Yohan-Ishara@users.noreply.github.com>
2026-01-12 13:10:07 +0530

    Update CorsConfig.java

--- src/main/java/com/social/aisocialcontentgenerator/config/CorsConfig.java
+++ src/main/java/com/social/aisocialcontentgenerator/config/CorsConfig.java
@@ -18,7 +18,8 @@ public class CorsConfig {
 
         config.setAllowedOrigins(List.of(
                 "http://localhost:5173",
-                "https://ai-social-content-gen-omega.vercel.app"
+                "https://ai-social-content-gen-omega.vercel.app",
+                "https://social-content-generator-five.vercel.app"
         ));
         config.setAllowedMethods(List.of(
                 "GET", "POST", "PUT", "DELETE", "OPTIONS"
